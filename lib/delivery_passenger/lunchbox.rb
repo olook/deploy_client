b48338dca0a1ccb20de24643efdb01be6a9ec665
zanst <me@zan.st>
2011-12-20 19:43:02 -0200

    UPDATED: DeliveryPassenger::Lunchbox

--- lib/delivery_passenger/lunchbox.rb
+++ lib/delivery_passenger/lunchbox.rb
@@ -1,20 +1,56 @@
 module DeliveryPassenger
   class Lunchbox
-    attr_reader :schedule_file
+    attr_reader :schedule_file, :date
 
     def initialize
       @schedule_file = File.dirname(__FILE__) + '/../../config/schedule.rb'
     end
 
+    def dispatcher(type, crondate)
+      case type
+        when 'deploy'
+          set_crondate_on_config_file(crondate)
+          deploy
+        when 'rollback'
+          rollback
+      end
+    end
+
     def deploy
-      options          = {}
-      options[:file]   = @schedule_file
-      options[:user]   = 'root'
-      options[:update] = true
+      options = {}
+      options[:file], options[:user], options[:update] = @schedule_file, 'root', true
       Whenever::CommandLine.execute(options)
     end
 
     def rollback
+    end
+
+    private
+
+    def set_crondate_on_config_file(crondate)
+      parse_raw_crondate(crondate)
+      last_crondate = load_current_crondate
+      last_crondate['crondate'] = parse_raw_crondate(crondate)
+    end
+
+    #def write_yaml(hash)
+    #  File.open(@schedule_file, 'w') do |f|
+    #    f.write(generate_yaml(hash))
+    #  end
+    #end
+
+    #def generate_yaml(hash)
+    #  method = hash.respond_to?(:ya2yaml) ? :ya2yaml : :to_yaml
+    #  string = hash.deep_stringify_keys.send(method)
+    #end
+
+    def load_current_crondate
+      crondate_file = File.dirname(__FILE__) + '/../../config/crondate.yml'
+      YAML.load_file(crondate_file)
+    end
+
+    def parse_raw_crondate(crondate)
+      crondate.strftime("%M %H %d %m")
     end
   end
 end
