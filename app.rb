85e59742346567d829e26723ada83bc69a8951db
zanst <me@zan.st>
2011-12-21 16:51:13 -0200

    added port to sinatra app

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require './lib/delivery_passenger'
 
+configure do
+  set :port, 3334
+end
+
 post '/receptor' do
   DeliveryPassenger::Lunchbox.new.dispatcher(params[:type], params[:date])
 end
